575383e26083e0ba5afc4003a551ff58c1a5bebe
G. S. Averill <gsa@logmach.com>
2014-12-01 19:33:51 -0700

    Good progress.

--- plot1.R
+++ plot1.R
@@ -1,5 +1,22 @@
+#
 # R script
 #
-# G. S. Averill
-# Colorado Logic Machines, LLC
-# Copyright 2014, all rights reserved
+
+# Path to the source data
+sourceDataURL <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
+
+# Name of the file to extract from the .zip archive
+sourceDataFileName <- "household_power_consumption.txt"
+
+# Download the .zip archive, extract the data, and load into a data frame.
+tmpFile <- tempfile()
+download.file(sourceDataURL,tmpFile, method="curl")
+hpcData <- read.table(unz(tmpFile, sourceDataFileName),
+                      header=TRUE, 
+                      sep=";",
+                      quote="",
+                      na.strings ="?"
+                     )
+unlink(tmpFile)
+
+
