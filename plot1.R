1c4d28f965d8ab1dd6e0a34f95a74b7f7dccf005
G. S. Averill <gsa@logmach.com>
2014-12-01 23:40:38 -0700

    Cleaned up data and converted dates and times.

--- plot1.R
+++ plot1.R
@@ -2,10 +2,10 @@
 # R script
 #
 
-# Path to the source data
+# Path to the source data.
 sourceDataURL <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
 
-# Name of the file to extract from the .zip archive
+# Name of the file to extract from the .zip archive.
 sourceDataFileName <- "household_power_consumption.txt"
 
 # Download the .zip archive, extract the data, and load into a data frame.
@@ -19,4 +19,15 @@ hpcData <- read.table(unz(tmpFile, sourceDataFileName),
                      )
 unlink(tmpFile)
 
+# Convert the Date and Time column values, which read.table brought in as 
+# factors, into Date and POSIXlt types.
+hpcData$Time <- strptime(paste(hpcData$Date, hpcData$Time), 
+                         paste("%d/%m/%Y",   "%H:%M:%S"  )
+                        )
 
+hpcData$Date <- as.Date(hpcData$Date, "%d/%m/%Y")
+
+# Grab the subset of the data frame that covers February 1-2, 2007.
+beginDate <- as.Date("2007-02-01")
+endDate   <- as.Date("2007-02-02")
+hpcData   <- hpcData[hpcData$Date %in% c(beginDate, endDate),]
