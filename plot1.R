7592d67e0019f244b81454c1121ca733cac85c32
G. S. Averill <gsa@logmach.com>
2014-12-02 01:41:42 -0700

    Finished plot1.R.

--- plot1.R
+++ plot1.R
@@ -1,6 +1,11 @@
+###############################################################################
+# R Script
 #
-# R script
-#
+# This script generates Plot 1, which is part of Course Project 1.
+# Class: Exploratory Data Analysis, Coursera Data Science Sequence
+# 
+# Author: G. S. Averill
+###############################################################################
 
 # Path to the source data.
 sourceDataURL <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
@@ -8,26 +13,41 @@ sourceDataURL <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_po
 # Name of the file to extract from the .zip archive.
 sourceDataFileName <- "household_power_consumption.txt"
 
-# Download the .zip archive, extract the data, and load into a data frame.
+# Download the .zip archive, extract the data file, and load the data from the
+# file into a data frame.
 tmpFile <- tempfile()
-download.file(sourceDataURL,tmpFile, method="curl")
+download.file(sourceDataURL,tmpFile, method = "curl")
 hpcData <- read.table(unz(tmpFile, sourceDataFileName),
-                      header=TRUE, 
-                      sep=";",
-                      quote="",
-                      na.strings ="?"
+                      header = TRUE, 
+                      sep = ";",
+                      na.strings = "?" # NAs are represented by "?"s.
                      )
 unlink(tmpFile)
 
-# Convert the Date and Time column values, which read.table brought in as 
+# Convert the Date and Time column values, which read.table() brought in as 
 # factors, into Date and POSIXlt types.
 hpcData$Time <- strptime(paste(hpcData$Date, hpcData$Time), 
                          paste("%d/%m/%Y",   "%H:%M:%S"  )
                         )
-
 hpcData$Date <- as.Date(hpcData$Date, "%d/%m/%Y")
 
 # Grab the subset of the data frame that covers February 1-2, 2007.
 beginDate <- as.Date("2007-02-01")
 endDate   <- as.Date("2007-02-02")
-hpcData   <- hpcData[hpcData$Date %in% c(beginDate, endDate),]
+hpcData <- hpcData[((hpcData$Date >= beginDate) & (hpcData$Date <= endDate)), ]
+
+# Open a PNG graphics device.
+png(filename = "plot1.png", 
+    width = 480,
+    height = 480
+   )
+
+# Make the plot.
+hist(hpcData$Global_active_power,
+     col = "red1",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)"
+    )
+
+# Close the graphics device
+dev.off()
